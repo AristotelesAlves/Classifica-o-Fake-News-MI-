c3730f0058e4774b708fa0865d1382b4da84b68c
aristotelesAlves <arystotelys@gmail.com>
2024-11-27 19:08:10 -0300

    loading gif

--- static/script.js
+++ static/script.js
@@ -1,26 +1,34 @@
 document.getElementById('classificationForm').addEventListener('submit', async function(event) {
-    event.preventDefault();  // Impede o comportamento padrão de envio do formulário
+    event.preventDefault(); // Impede o comportamento padrão de envio do formulário
+
+    // Exibir o GIF de carregamento
+    const loadingElement = document.getElementById('loading');
+    loadingElement.style.display = 'flex';
 
     // Coletar os dados do formulário
     const texto = document.getElementById('texto').value;
 
-    // Enviar os dados para o servidor usando fetch
-    const response = await fetch('/classificar', {  // A URL deve ser relativa
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            texto: texto
-        })
-    });
+    try {
+        // Enviar os dados para o servidor usando fetch
+        const response = await fetch('/classificar', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ texto: texto })
+        });
 
-    // Verificar se a requisição foi bem-sucedida
-    if (response.ok) {
-        const result = await response.json();  // Receber a resposta JSON
-        // Exibir o resultado no HTML
-        document.getElementById('classificationResult').textContent = `Classe predita: ${result.prediction}`;
-    } else {
-        document.getElementById('classificationResult').textContent = 'Erro ao classificar o texto.';
+        if (response.ok) {
+            const result = await response.json(); // Receber a resposta JSON
+            // Exibir o resultado no HTML
+            document.getElementById('classificationResult').textContent = `Classe predita: ${result.prediction}`;
+        } else {
+            document.getElementById('classificationResult').textContent = 'Erro ao classificar o texto.';
+        }
+    } catch (error) {
+        document.getElementById('classificationResult').textContent = 'Erro ao processar a requisição.';
+    } finally {
+        // Ocultar o GIF de carregamento
+        loadingElement.style.display = 'none';
     }
 });
